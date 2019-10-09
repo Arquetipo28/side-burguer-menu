7cfb9c3dc463887d0e821416a7a9edde69a905cb
German Reynaga Araiza <german.reynagaa@gmail.com>
2019-10-09 00:31:58 -0500

    Allow users to toggle side menu

---
+++ assets/main.js
@@ -0,0 +1,11 @@
+var $sideMenu = $('#side-menu')
+$('#togle-btn').click(() => {
+  $sideMenu.toggleClass("side-open")
+})
+
+$(document).mouseup((e) => {
+  if ((!$sideMenu.is(e.target)) && $sideMenu.has(e.target).length === 0)
+  {
+    $sideMenu.toggleClass("side-open")
+  }
+});
